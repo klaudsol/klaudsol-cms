7f699a8b690155dd4280d4724a6b97d78f3eec03
Angeli Mercedes Taso <101687552+angeli-mercedes@users.noreply.github.com>
2022-10-17 19:28:12 +0800

    create new entry

--- backend/models/core/Entity.js
+++ backend/models/core/Entity.js
@@ -6,7 +6,7 @@ class Entity {
     static async find({entity_type_slug, id}) {
       const db = new DB();
 
-      const sql = `SELECT entities.id, entity_types.name, entity_types.slug, entities.slug, 
+      const sql = `SELECT entities.id, entity_types.id, entity_types.name, entity_types.slug, entities.slug, 
                   attributes.name, attributes.type, attributes.\`order\`,
         \`values\`.value_string, 
         \`values\`.value_long_string, 
@@ -31,6 +31,7 @@ class Entity {
       
       return data.records.map(([
           {longValue: id},
+          {longValue: entity_type_id},
           {stringValue: entity_type_name},
           {stringValue: entity_type_slug},
           {stringValue: entities_slug},
@@ -44,6 +45,7 @@ class Entity {
           {stringValue: value_double}
         ]) => ({
           id, 
+          entity_type_id,
           entity_type_name, 
           entity_type_slug, 
           entities_slug, 
@@ -61,7 +63,7 @@ class Entity {
     static async where({entity_type_slug}) {
       const db = new DB();
 
-      const sql = `SELECT entities.id, entity_types.name, entity_types.slug, entities.slug, 
+      const sql = `SELECT entities.id, entity_types.id, entity_types.name, entity_types.slug, entities.slug, 
                   attributes.name, attributes.type, attributes.\`order\`,
         \`values\`.value_string, 
         \`values\`.value_long_string, 
@@ -83,6 +85,7 @@ class Entity {
       
       return data.records.map(([
           {longValue: id},
+          {longValue: entity_type_id},
           {stringValue: entity_type_name},
           {stringValue: entity_type_slug},
           {stringValue: entities_slug},
@@ -96,6 +99,7 @@ class Entity {
           {stringValue: value_double}
         ]) => ({
           id, 
+          entity_type_id,
           entity_type_name, 
           entity_type_slug, 
           entities_slug, 
@@ -108,6 +112,50 @@ class Entity {
           value_datetime,
           value_double
         })); 
+  }
+
+  static async create({entries, columns, slug, entity_type_id}) {
+    const db = new DB();
+
+    let attributesValues = ``, insertAttributesSQL = ``;
+
+    let values =``, insertValuesSql = ``;
+    let dec = columns.length - 1;
+
+    const insertEntitiesSQL = `INSERT into entities (slug, entity_type_id) VALUES (:slug, :entity_type_id)`;
+    //const isEntities = true;    
+    
+    const isEntities = await db.exectuteStatement(insertEntitiesSQL, [
+      {name: 'slug', value:{stringValue: slug}},
+      {name: 'entity_type_id', value:{longValue: entity_type_id}},
+    ]);
+    
+    if(isEntities) {
+      
+    let temp = [];
+
+    columns.map((col, i) => {
+      entries[`${col}_type`] === 'text' ? temp.push({value_string: entries[col]}) : null;
+      entries[`${col}_type`] === 'textarea' ? temp.push({value_long_string: entries[col]}) : null;
+      entries[`${col}_type`] === 'float' ? temp.push({value_double: entries[col]}) : null;
+
+      attributesValues += ` ${i > 0 ? ',' : ''}('${col}', (SELECT ID from entities ORDER by id desc limit 1),  '${entries[`${col}_type`]}', ${i+1})`;
+      values += ` ${i > 0 ? ',' : ''}((SELECT ID from entities ORDER by id desc limit 1), (SELECT ID from attributes ORDER by id desc limit 1)-${dec},${entries[`${col}_type`] === 'text' ? entries[col] : null}, ${entries[`${col}_type`] === 'textarea' ? entries[col] : null}, ${entries[`${col}_type`] === 'float' ? entries[col] : null})`;
+      dec--;
+    })
+
+    insertAttributesSQL = `INSERT into attributes (name, entity_id, type, \`order\`) VALUES ${attributesValues}`;
+    
+    const isAttributes = await db.exectuteStatement(insertAttributesSQL, []);
+
+    if(isAttributes) {
+      insertValuesSql = `INSERT into \`values\` (entity_id, attribute_id, value_string, value_long_string, value_double) VALUES ${values}`;
+      const isValues = await db.exectuteStatement(insertValuesSql, []);
+    }
+
+    }
+
+    return true;
   }
 }
 
