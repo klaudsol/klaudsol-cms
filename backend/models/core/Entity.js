c1216c8aaa704230ebb678f0d1792bd6bbab6f62
Ardee Aram <ardee.aram@safelinkhub.com>
2022-10-18 08:22:16 +0000

    Code cleanup

--- backend/models/core/Entity.js
+++ backend/models/core/Entity.js
@@ -1,5 +1,4 @@
 import DB from '@backend/data_access/DB';
-import Id from 'pages/api/[entity_type_slug]/[id]';
 
 
 class Entity {
@@ -118,85 +117,65 @@ class Entity {
   //Work in progress
   static async create({entries, columns, slug, entity_type_id}) {
     
-    //console.error(entries);
-    
-    //console.error(columns);
-    
-    //console.error(slug);
-    
     const db = new DB();
 
-    let attributesValues = ``, insertAttributesSQL = ``;
-
-    let values =``, insertValuesSql = ``;
-    let dec = columns.length - 1;
-    
     //TODO: start transaction
     
-    //Column Introspection
-    const entityIntrospection = `SELECT name, type FROM entity_type_attributes 
-      WHERE entity_type_id = :entity_type_id ORDER by \`order\``;
-    
-    const _columns = await db.executeStatement(entityIntrospection, [
-      {name: 'entity_type_id', value:{longValue: entity_type_id}},
-    ]);
-    
-    const attributes = _columns.records.reduce((collection, record) => {
-      const [
-        attributeName,
-        attributeType,
-      ] = record;
-      
-      return {
-        ...collection,
-        [attributeName.stringValue]: {
-          type: attributeType.stringValue   
-        } 
-      }    
-      
-    }, {}); 
-    
-    console.error(attributes);
     
     //Insert Entity
     const insertEntitiesSQL = 'INSERT into entities (slug, entity_type_id) VALUES (:slug, :entity_type_id)';
     
-    //await db.executeStatement(insertEntitiesSQL, [
-    //  {name: 'slug', value:{stringValue: slug}},
-    //  {name: 'entity_type_id', value:{longValue: entity_type_id}},
-    //]);
+    await db.executeStatement(insertEntitiesSQL, [
+      {name: 'slug', value:{stringValue: slug}},
+      {name: 'entity_type_id', value:{longValue: entity_type_id}},
+    ]);
     
     
     const {records: [[{longValue: lastInsertedEntityID}]] } = await db.executeStatement('SELECT LAST_INSERT_ID()');
     console.error(lastInsertedEntityID);
     
-    //Copy Attributes from Entity Type Attributes
-    const insertAttributes = 'INSERT INTO attributes(name, entity_id, '
+     //Attribute Introspection
+    const entityIntrospectionSQL = `SELECT id, name, type FROM attributes 
+      WHERE entity_type_id = :entity_type_id ORDER by \`order\``;
     
+    const attributes = await db.executeStatement(entityIntrospectionSQL, [
+      {name: 'entity_type_id', value:{longValue: entity_type_id}},
+    ]);
+    
+    const valueBatchParams = attributes.records.reduce((collection, record) => {
+      const [
+        {longValue: attributeId},
+        {stringValue: attributeName},
+        {stringValue: attributeType},
+      ] = record;
       
-    let temp = [];
-
-    columns.map((col, i) => {
-      entries[`${col}_type`] === 'text' ? temp.push({value_string: entries[col]}) : null;
-      entries[`${col}_type`] === 'textarea' ? temp.push({value_long_string: entries[col]}) : null;
-      entries[`${col}_type`] === 'float' ? temp.push({value_double: entries[col]}) : null;
-
-      attributesValues += ` ${i > 0 ? ',' : ''}('${col}', (SELECT ID from entities ORDER by id desc limit 1),  '${entries[`${col}_type`]}', ${i+1})`;
-      values += ` ${i > 0 ? ',' : ''}((SELECT ID from entities ORDER by id desc limit 1), (SELECT ID from attributes ORDER by id desc limit 1)-${dec},${entries[`${col}_type`] === 'text' ? entries[col] : null}, ${entries[`${col}_type`] === 'textarea' ? entries[col] : null}, ${entries[`${col}_type`] === 'float' ? entries[col] : null})`;
-      dec--;
-    })
-
-/*
-    insertAttributesSQL = `INSERT into attributes (name, entity_id, type, \`order\`) VALUES ${attributesValues}`;
+      return [ 
+        ...collection,
+        [
+          {name: 'entity_id', value: {longValue: lastInsertedEntityID}},
+          {name: 'attribute_id', value: {longValue: attributeId}},
+          {name: 'value_string', value: attributeType == 'text' ? {stringValue: entries[attributeName]} : {isNull: true}},
+          {name: 'value_long_string', value:  attributeType == 'textarea' ? {stringValue: entries[attributeName]} : {isNull: true}},
+          {name: 'value_double', value:  attributeType == 'float' ? {doubleValue: entries[attributeName]} : {isNull: true}},
+        ]
+      ]    
+      
+    }, []); 
     
-    const isAttributes = await db.executeStatement(insertAttributesSQL, []);
+    
+    console.error(JSON.stringify(valueBatchParams));
+    
+    //Insert Values by batch
+    const insertValuesBatchSQL = `INSERT INTO \`values\`(entity_id, attribute_id,
+      value_string, value_long_string, value_double  
+    ) VALUES (:entity_id, :attribute_id, :value_string, :value_long_string, :value_double) 
+    `; 
+    
+    await db.batchExecuteStatement(insertValuesBatchSQL,valueBatchParams);
+    
 
-
-    insertValuesSql = `INSERT into \`values\` (entity_id, attribute_id, value_string, value_long_string, value_double) VALUES ${values}`;
-    const isValues = await db.executeStatement(insertValuesSql, []);
     
     //TODO: end transaction
-*/
 
     return true;
   }
