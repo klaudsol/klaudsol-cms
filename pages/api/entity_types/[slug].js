6a273283c6a6f219cac2a24b14ad6c53806274fa
Ardee Aram <ardee.aram@gmail.com>
2022-12-05 05:30:30 +0800

    Add license terrms

--- pages/api/entity_types/[slug].js
+++ pages/api/entity_types/[slug].js
@@ -1,3 +1,28 @@
+/**
+ * MIT License
+
+Copyright (c) 2022 KlaudSol Philippines, Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+**/
+
 import EntityType from '@backend/models/core/EntityType';
 import { withSession } from '@/lib/Session';
 import { defaultErrorHandler } from '@/lib/ErrorHandler';
@@ -13,6 +38,8 @@ async function handler(req, res) {
     switch(req.method) {
       case "GET":
         return get(req, res); 
+        case "DELETE":
+          return del(req, res); 
       default:
         throw new Error(`Unsupported method: ${req.method}`);
     }
@@ -56,4 +83,8 @@ async function handler(req, res) {
     catch (error) {
       await defaultErrorHandler(error, req, res);
     }
+  }
+
+
+  async function del(req, res) { 
   }
