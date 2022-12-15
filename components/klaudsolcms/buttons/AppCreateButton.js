e1372a1056ee7c21446fe34d9e52927cb80b7f26
bienjam <jamonbienvenido@gmail.com>
2022-12-15 22:27:04 +0800

    Added new line

--- components/klaudsolcms/buttons/AppCreateButton.js
+++ components/klaudsolcms/buttons/AppCreateButton.js
@@ -1,5 +1,6 @@
 import { FaPlus } from "react-icons/fa";
 import Link from 'next/link';
+
 const AppCreateButton = ({title, link, onClick}) => (
         <button className="btn_create_entry" onClick={onClick}> 
             <FaPlus className="icon_general"/>
