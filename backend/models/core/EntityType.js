8ca7d3d4ff0a700528bb42f7b8d08ef65cef5e5a
john <jaservo@up.edu.ph>
2023-01-11 07:46:10 +0800

    Capitalize sql queries

--- backend/models/core/EntityType.js
+++ backend/models/core/EntityType.js
@@ -5,7 +5,7 @@ class EntityTypes {
   static async all() {
     const db = new DB();
 
-    const sql = `SELECT entity_types.id, entity_types.name, entity_types.slug from entity_types`;
+    const sql = `SELECT entity_types.id, entity_types.name, entity_types.slug FROM entity_types`;
 
     const data = await db.executeStatement(sql, []);
 
@@ -26,7 +26,7 @@ class EntityTypes {
   static async findBySlug(slug, { db: _db } = {}) {
     const db = _db ?? new DB();
 
-    const sql = `SELECT entity_types.id, entity_types.name, entity_types.slug from entity_types WHERE entity_types.slug = :slug LIMIT 1`;
+    const sql = `SELECT entity_types.id, entity_types.name, entity_types.slug FROM entity_types WHERE entity_types.slug = :slug LIMIT 1`;
 
     const data = await db.executeStatement(sql, [
       { name: "slug", value: { stringValue: slug } },
@@ -99,7 +99,7 @@ class EntityTypes {
     const db = new DB();
 
     const insertEntitiesSQL =
-      "INSERT into entity_types (slug, name) VALUES (:slug, :name)";
+      "INSERT INTO entity_types (slug, name) VALUES (:slug, :name)";
 
     await db.executeStatement(insertEntitiesSQL, [
       { name: "slug", value: { stringValue: slug } },
@@ -114,7 +114,7 @@ class EntityTypes {
     const db = new DB();
 
     const insertEntitiesSQL =
-      "INSERT into entity_types (slug, name) VALUES (:slug, :name)";
+      "INSERT INTO entity_types (slug, name) VALUES (:slug, :name)";
 
     await db.executeStatement(insertEntitiesSQL, [
       { name: "slug", value: { stringValue: slug } },
@@ -127,7 +127,7 @@ class EntityTypes {
 
   static async delete({ slug }) {
     const db = new DB();
-    const deleteEntityTypesSQL = "DELETE from entity_types where slug = :slug";
+    const deleteEntityTypesSQL = "DELETE FROM entity_types WHERE slug = :slug";
 
     let executeStatementParam = [
       { name: "slug", value: { stringValue: slug } },
