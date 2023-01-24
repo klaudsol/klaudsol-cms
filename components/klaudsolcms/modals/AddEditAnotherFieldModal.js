480808488b02d6bcdb2b558c7c58e5cd004cb259
Angeli Mercedes Taso <101687552+angeli-mercedes@users.noreply.github.com>
2023-01-24 13:07:57 +0800

    Klaudsol-Cms #124: Adjusted the width of input-boxes (#126) (#141)
    * Adjusted the width of input-boxes
    
    * separated the css in scss file
    
    Co-authored-by: Ardee Aram <ardeearam@users.noreply.github.com>
    Co-authored-by: Bien Jamon <108235250+bienjam@users.noreply.github.com>

--- components/klaudsolcms/modals/AddEditAnotherFieldModal.js
+++ components/klaudsolcms/modals/AddEditAnotherFieldModal.js
@@ -17,29 +17,27 @@ export const EDIT_MODE = "EDIT_MODE";
       onClose={onClose}
       onClick={onClick}
       isLoading={isLoading}
-      modalTitle={mode === ADD_MODE ? "Add another field" : "Edit field"}
-      buttonTitle={mode === ADD_MODE ? "Add" : "Update"}
-    >
-      {/* TODO: */}
-      {/* <AddFieldBody /> */}
-      <Formik {...formParams}>
-        <Form>
-          <table id="table_general">
-            {/*table head*/}
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Type</th>
-                <th>Order</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>
-                  <Field name="name" className="input_text" />
-                </td>
-                <td>
-                  <Field name="type" component="select" className="input_text">
+      modalTitle={mode === ADD_MODE ? 'Add another field' : 'Edit field'} 
+      buttonTitle={mode === ADD_MODE ? 'Add': 'Update'}> 
+    {/* TODO: */}
+    {/* <AddFieldBody /> */}
+    <Formik {...formParams}>
+      <Form>
+        
+        <table id="table_general">
+        {/*table head*/}
+        <thead> 
+            <tr>
+              <th>Name</th>
+              <th>Type</th>
+              <th>Order</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+              <td><Field name='name' className='input_text' /></td>
+              <td className='table-box'>
+                  <Field name='type' component='select' className='input_text'>
                     {/*TODO: Make dynamic please */}
                     <option value="text">Text</option>
                     <option value="textarea">Text Area</option>
@@ -47,16 +45,15 @@ export const EDIT_MODE = "EDIT_MODE";
                     <option value="image">Image</option>
                     <option value="float">Number</option>
                   </Field>
-                </td>
-                <td>
-                  <Field name="order" className="input_text" type="number" />
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </Form>
-      </Formik>
-    </AppModal>
+              </td>
+              <td className='table-box'><Field name='order' className='input_text'  type='number' /></td>
+            </tr>
+        </tbody>
+    </table>
+      </Form>
+    </Formik>
+  </AppModal>
+
   );
 
   export default AddEditAnotherFieldModal;
