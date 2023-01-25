c975ae01fa4f78860475b4548ec9c5489d8a9868
john <jaservo@up.edu.ph>
2023-01-25 12:57:09 +0800

    Fix server error when url is non existent string

--- pages/api/[entity_type_slug]/[id].js
+++ pages/api/[entity_type_slug]/[id].js
@@ -56,23 +56,13 @@ async function get(req, res) {
     const { entity_type_slug, id: slug } = req.query;
 
     const stringData = await Entity.findBySlug({ entity_type_slug, slug });
-    const intData =
-      parseInt(slug) && stringData.length === 0
-        ? await Entity.find({ entity_type_slug, id: slug })
-        : "";
+    const intData = parseInt(slug) && stringData.length === 0
+                    ? await Entity.find({ entity_type_slug, id: slug })
+                    : "";
 
-    const rawData = stringData.length > 0 ? stringData : intData;
-
-    /* // If user typed in the id instead of the slug */
-    /* // If slug is equal to one of the IDs, prioritize slug */
-    /* if (parseInt(slug) && rawData.length === 0) { */
-    /*   const item = await Entity.find({ entity_type_slug, id: slug }); */
-    /**/
-    /*   if (item.length !== 0) { */
-    /*     const itemSlug = item[0].entities_slug; */
-    /*     return res.redirect(`/api/${entity_type_slug}/${itemSlug}`); */
-    /*   } */
-    /* } */
+    const rawData = stringData.length > 0 ? stringData :
+                    intData.length > 0 ? intData :
+                    [];
 
     const initialFormat = {
       data: {},
