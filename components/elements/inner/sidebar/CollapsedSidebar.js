c7b06f2f79f6d55e6a68fd5160225a65fd50fb7e
john <jaservo@up.edu.ph>
2023-01-26 14:52:51 +0800

    Returned classes

--- components/elements/inner/sidebar/CollapsedSidebar.js
+++ components/elements/inner/sidebar/CollapsedSidebar.js
@@ -27,9 +27,9 @@ const CollapsedSidebar = ({sidebarButtons, firstName, lastName, defaultEntityTyp
         {/*<CIcon className="sidebar-brand-full" icon={logoNegative} height={35} />
         <CIcon className="sidebar-brand-narrow" icon={sygnet} height={35} />
         */}
-         <Link href='/admin' passHref>
+         <Link href='/admin' className='sidebar_header' passHref>
             <b>  CMS  </b>
-          </Link>
+         </Link>
           
         
       </CSidebarBrand>
