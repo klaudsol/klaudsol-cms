12baf867db9f858aba44ee144f8958091190b9db
john <jaservo@up.edu.ph>
2023-01-30 08:32:31 +0800

    Format method

--- backend/models/core/Entity.js
+++ backend/models/core/Entity.js
@@ -1,4 +1,5 @@
 import DB from '@backend/data_access/DB';
+import util from 'util';
 
 
 class Entity {
@@ -171,15 +172,19 @@ class Entity {
 
     //Work in progress
     static async create({slug, entity_type_id, ...entry}) {
-
-    
       const db = new DB();
   
       //TODO: start transaction
       
       
       //Insert Entity
-      const insertEntitiesSQL = 'INSERT into entities (slug, entity_type_id) VALUES (:slug, :entity_type_id)';
+      const insertEntitiesSQL = `INSERT into entities (
+            slug, 
+            entity_type_id
+        ) VALUES (
+            :slug, 
+            :entity_type_id
+        )`;
       
       await db.executeStatement(insertEntitiesSQL, [
         {name: 'slug', value:{stringValue: slug}},
@@ -187,48 +192,63 @@ class Entity {
       ]);
       
       
-      const {records: [[{longValue: lastInsertedEntityID}]] } = await db.executeStatement('SELECT LAST_INSERT_ID()');
+      const { records: [ [ { longValue: lastInsertedEntityID } ] ] } = await db.executeStatement('SELECT LAST_INSERT_ID()');
       console.error(lastInsertedEntityID);
       
        //Attribute Introspection
-      const entityIntrospectionSQL = `SELECT id, name, type FROM attributes 
-        WHERE entity_type_id = :entity_type_id ORDER by \`order\``;
+      const entityIntrospectionSQL = `SELECT id, name, type 
+        FROM attributes 
+        WHERE 
+            entity_type_id = :entity_type_id 
+        ORDER by \`order\``;
       
       const attributes = await db.executeStatement(entityIntrospectionSQL, [
-        {name: 'entity_type_id', value:{longValue: entity_type_id}},
+        { name: 'entity_type_id', value:{ longValue: entity_type_id } },
       ]);
       
       const valueBatchParams = attributes.records.reduce((collection, record) => {
         const [
-          {longValue: attributeId},
-          {stringValue: attributeName},
-          {stringValue: attributeType},
+          { longValue: attributeId },
+          { stringValue: attributeName },
+          { stringValue: attributeType },
         ] = record;
         
         return [ 
           ...collection,
           [
-            {name: 'entity_id', value: {longValue: lastInsertedEntityID}},
-            {name: 'attribute_id', value: {longValue: attributeId}},
+            { name: 'entity_id', value: { longValue: lastInsertedEntityID } },
+            { name: 'attribute_id', value: { longValue: attributeId } },
             //Refactor to encapsulate type switch
-            {name: 'value_string', value: (attributeType == 'text' || attributeType == 'image' || attributeType == 'link') ? {stringValue: entry[attributeName]} : {isNull: true}},
-            {name: 'value_long_string', value:  attributeType == 'textarea' ? {stringValue: entry[attributeName]} : {isNull: true}},
-            {name: 'value_double', value:  attributeType == 'float' ? {doubleValue: entry[attributeName]} : {isNull: true}},
+            { name: 'value_string', value: ( attributeType == 'text' || attributeType == 'image' || attributeType == 'link' ) ? { stringValue: entry[attributeName] } : { isNull: true } },
+            { name: 'value_long_string', value:  attributeType == 'textarea' ? { stringValue: entry[attributeName] } : { isNull: true } },
+            { name: 'value_double', value:  attributeType == 'float' ? { doubleValue: entry[attributeName] } : { isNull: true } },
           ]
-        ]    
-        
+        ]
       }, []); 
       
       
       //Insert Values by batch
-      const insertValuesBatchSQL = `INSERT INTO \`values\`(entity_id, attribute_id,
-        value_string, value_long_string, value_double  
-      ) VALUES (:entity_id, :attribute_id, :value_string, :value_long_string, :value_double) 
-      `; 
+      const insertValuesBatchSQL = `INSERT INTO \`values\`(
+            entity_id, 
+            attribute_id,
+            value_string, 
+            value_long_string, 
+            value_double  
+        ) VALUES (
+            :entity_id, 
+            :attribute_id, 
+            :value_string, 
+            :value_long_string, 
+            :value_double
+        )`; 
       
       await db.batchExecuteStatement(insertValuesBatchSQL,valueBatchParams);
-      
-  
+        
+      const imageAttributes = attributes.records.filter((attribute) => attribute[2].stringValue === 'image');
+
+
+        /* console.log(util.inspect(attributes, false, null, true)); */
+
       
       //TODO: end transaction
   
