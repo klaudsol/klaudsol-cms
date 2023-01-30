a8e92215bad8d9fb9dd85340854adf97155db254
Norlant1 <jobenbarrera@gmail.com>
2023-01-30 14:41:31 +0800

    final commit

--- backend/models/core/Entity.js
+++ backend/models/core/Entity.js
@@ -3,72 +3,14 @@ import DB from '@backend/data_access/DB';
 
 class Entity {
 
-    static async find({entity_type_slug, id}) {
-      const db = new DB();
-
-      const sql = `SELECT entities.id, entity_types.id, entity_types.name, entity_types.slug, entities.slug, 
-                  attributes.name, attributes.type, attributes.\`order\`,
-                  \`values\`.value_string, 
-                  \`values\`.value_long_string, 
-                  \`values\`.value_integer, 
-                  \`values\`.value_datetime, 
-                  \`values\`.value_double                       
-                  FROM entities
-                  LEFT JOIN entity_types ON entities.entity_type_id = entity_types.id
-                  LEFT JOIN attributes ON attributes.entity_type_id = entity_types.id
-                  LEFT JOIN \`values\` ON values.entity_id = entities.id AND values.attribute_id = attributes.id
-                  WHERE 
-                      entity_types.slug = :entity_type_slug AND 
-                      entities.id = :id
-                  ORDER BY attributes.\`order\` ASC
-                  `;
-                
-      const data = await db.executeStatement(sql, [
-          {name: 'entity_type_slug', value:{stringValue: entity_type_slug}},
-          {name: 'id', value:{longValue: id}},
-      ]);
-      
-      return data.records.map(([
-          {longValue: id},
-          {longValue: entity_type_id},
-          {stringValue: entity_type_name},
-          {stringValue: entity_type_slug},
-          {stringValue: entities_slug},
-          {stringValue: attributes_name},
-          {stringValue: attributes_type},
-          {longValue: attributes_order},
-          {stringValue: value_string},
-          {stringValue: value_long_string},
-          {longValue: value_integer},
-          {stringValue: value_datetime},
-          {stringValue: value_double}
-        ]) => ({
-          id, 
-          entity_type_id,
-          entity_type_name, 
-          entity_type_slug, 
-          entities_slug, 
-          attributes_name, 
-          attributes_type, 
-          attributes_order, 
-          value_string,
-          value_long_string,
-          value_integer,
-          value_datetime,
-          value_double
-        })); 
-    }
-
-    static async findBySlug({entity_type_slug, slug}) {
-      const db = new DB();
-       
-      function isNumber(str) {
+    static async findBySlugOrId({entity_type_slug, slug}) {
+      const db = new DB();      
+     function isNumber(str) {
         return !isNaN(str);
       }
-
      const propertyType = isNumber(slug) ? "longValue" : "stringValue" 
      const conditionType = isNumber(slug) ? "entities.id" : "entities.slug";
-     
+
       const sql = `SELECT entities.id, entity_types.id, entity_types.name, entity_types.slug, entities.slug, 
                   attributes.name, attributes.type, attributes.\`order\`,
                   \`values\`.value_string, 
--- pages/api/[entity_type_slug]/[id].js
+++ pages/api/[entity_type_slug]/[id].js
@@ -55,19 +55,9 @@ async function get(req, res) {
   try {
     const { entity_type_slug, id: slug } = req.query;
 
-    const rawData = await Entity.findBySlug({ entity_type_slug, slug });
+    const rawData = await Entity.findBySlugOrId({ entity_type_slug, slug });
 
-    // If user typed in the id instead of the slug
-    // If slug is equal to one of the IDs, prioritize slug
-    // if(parseInt(slug) && rawData.length === 0) {
-    //     const item = await Entity.find({entity_type_slug, id: slug});
-
-    //     if(item.length !== 0) {
-    //         const itemSlug = item[0].entities_slug;
-    //         return res.redirect(`/api/${entity_type_slug}/${itemSlug}`);
-    //     }
-    // }
-
+    
     const initialFormat = {
       data: {},
       metadata: {
