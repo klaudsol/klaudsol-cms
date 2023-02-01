b61dfa84c44248f947ff306af0f6cc55cba560b3
john <jaservo@up.edu.ph>
2023-02-01 11:32:31 +0800

    Fix API error

--- components/EntityAttributeValue.js
+++ components/EntityAttributeValue.js
@@ -6,6 +6,8 @@
       case 'textarea':
         return item.value_long_string;
       case 'image':
+        if(!item.value_string) return;
+        
         const bucketBaseUrl = process.env.AWS_S3_BASE_URL;
 
         const key = item.value_string;
