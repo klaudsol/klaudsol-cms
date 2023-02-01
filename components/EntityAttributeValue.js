01ca0f8f1a0777d4fe08657c4049bd87b2792078
john <jaservo@up.edu.ph>
2023-02-01 10:32:09 +0800

    Update response of API

--- components/EntityAttributeValue.js
+++ components/EntityAttributeValue.js
@@ -6,12 +6,13 @@
       case 'textarea':
         return item.value_long_string;
       case 'image':
-        if(!item.value_string) return;
         const bucketBaseUrl = process.env.AWS_S3_BASE_URL;
 
+        const key = item.value_string;
+        const name = key.substring(key.indexOf('_') + 1);
         const link = `${bucketBaseUrl}/`
 
-        return { name: item.value_string, link };
+        return { name, key, link };
       case 'float':
         //TODO: Find a more accurate representation of float
         return Number(item.value_double);
