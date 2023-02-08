3a7637fb78aaf0041ee57ee062cf506b5accd620
Norlant1 <109654448+Norlant1@users.noreply.github.com>
2023-02-08 13:12:40 +0800

    [Klaudsol-CMS-#122] Bug selected collection type is incorrect when clicking a specific entry (#195)
    * initial commit
    
    * final commit
    
    * fix
    
    * fixed bug

--- components/layouts/ContentManagerLayout.js
+++ components/layouts/ContentManagerLayout.js
@@ -1,13 +1,12 @@
 import { AppContent, AppSidebar } from '@/components/elements/inner/index';
 import ClientSessionHandler from '@/components/ClientSessionHandler';
 import ContentManagerSubMenu from 'components/elements/inner/ContentManagerSubMenu';
-const ContentManagerLayout = ({children}) => {
+const ContentManagerLayout = ({children,currentTypeSlug}) => {
   return (
     <ClientSessionHandler>
         <AppSidebar />
-
         <div className="wrapper d-flex align-items-start justify-content-start min-vh-100 bg-light">
-          <ContentManagerSubMenu title='Content'/>
+          <ContentManagerSubMenu title='Content' currentTypeSlug={currentTypeSlug}/>
             <div className="body flex-grow-1 px-3 main-container">
               <AppContent>
                     {children}
