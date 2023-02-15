22e6e4e70c76e141b109f5aa75849af829b45d62
Norlant1 <109654448+Norlant1@users.noreply.github.com>
2023-02-15 20:01:06 +0800

    Update [id].js
    resolved comments

--- pages/admin/content-manager/[entity_type_slug]/[id].js
+++ pages/admin/content-manager/[entity_type_slug]/[id].js
@@ -145,7 +145,7 @@ export default function Type({ cache }) {
 
   const getS3Keys = (files) => {
     const fileKeys = Object.keys(files);
-    const s3Keys = fileKeys.filter((file) => state.values[file].key);
+    const s3Keys = fileKeys.filter(file => state.values[file].key);
     
     return s3Keys;
   };
