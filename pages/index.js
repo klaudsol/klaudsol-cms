1b40898c0c2271c7765770679aab8b38b4fa9339
Norlant1 <jobenbarrera@gmail.com>
2023-02-18 21:11:58 +0800

    added serversidelogout

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import FrontPageLayout from "@/components/layouts/FrontPageLayout";
 import { withIronSessionSsr } from "iron-session/next";
 import { sessionOptions } from "@/lib/Session";
 import { slsFetch } from "@/components/Util";
+import { serverSideLogout } from "@/lib/Session";
 
 export default function Index(props) {
   return <FrontPageLayout {...props}></FrontPageLayout>;
@@ -10,12 +11,19 @@ export default function Index(props) {
 
 export const getServerSideProps = withIronSessionSsr(
   async function getServerSideProps({ req, res }) {
+
     try {
       const response = await slsFetch(
         `${process.env.FRONTEND_URL}/api/settings/mainlogo`
       );
       var { data } = await response.json();
     } catch (err) {}
+
+  try{
+    if(req.session?.cache?.forcePasswordChange){     
+      await serverSideLogout(req)
+    }
+  } catch(err){}
 
     let homepage = req.session?.cache?.homepage;
     if ((homepage && !req.session?.cache?.forcePasswordChange)) {
@@ -25,7 +33,8 @@ export const getServerSideProps = withIronSessionSsr(
           destination: `/${homepage}`,
         },
       };
-    } else {
+    } 
+    else {
       return {
         props: { logo: data ?? {} },
       };
