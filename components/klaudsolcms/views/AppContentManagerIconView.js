960dce7d78a7aaaf7ef3113a1aee471948aae00f
johnservo <jaservo@up.edu.ph>
2023-03-22 16:01:12 +0800

    Fix overflow and add title

--- components/klaudsolcms/views/AppContentManagerIconView.js
+++ components/klaudsolcms/views/AppContentManagerIconView.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import Image from "next/image";
+import NextImage from "next/image";
 import { useCapabilities } from "@/components/hooks";
 import { readContents } from "@/lib/Constants";
 import defaultImage from "@/public/default-image.svg";
@@ -17,35 +17,39 @@ const AppContentManagerIconView = ({ columns, entries, entity_type_slug }) => {
     return imageLink;
   };
 
+  const getTitle = (entry) => {
+    const key = Object.keys(entry).find(
+      (item) => item !== "slug" && typeof entry[item] === "string"
+    );
+
+    return entry[key];
+  };
+
   return (
     <div className="card__container">
-      {entries.map((entry) => {
-        const imageLink = findImage(entry);
-
-        return (
-          <div className="card__item" key={entry.id}>
-            <Link
-              href={`/admin/content-manager/${entity_type_slug}/${entry.id}`}
-              passHref
-              legacyBehavior
-              disabled={!capabilities.includes(readContents)}
-            >
-              <div className="card__image-container">
-                <Image
-                  src={imageLink ?? defaultImage}
-                  alt={entry.slug}
-                  className="card__image"
-                  fill
-                />
-              </div>
-            </Link>
-            <div className="card__data">
-              <div>{entry.id}</div>
-              <div>{entry.slug}</div>
+      {entries.map((entry) => (
+        <div className="card__item" key={entry.id}>
+          <Link
+            href={`/admin/content-manager/${entity_type_slug}/${entry.id}`}
+            passHref
+            legacyBehavior
+            disabled={!capabilities.includes(readContents)}
+          >
+            <div className="card__image-container">
+              <NextImage
+                src={findImage(entry) ?? defaultImage}
+                alt={entry.slug}
+                className="card__image"
+                sizes={290}
+                fill
+              />
             </div>
+          </Link>
+          <div className="card__data--container">
+            <div className="card__data">{getTitle(entry)}</div>
           </div>
-        );
-      })}
+        </div>
+      ))}
     </div>
   );
 };
