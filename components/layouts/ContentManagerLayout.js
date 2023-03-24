b5b6c0af927bbdfc7c52786a72b446349079b73e
Ardee Aram <ardee.aram@gmail.com>
2023-03-24 10:30:29 +0800

    Simplified one-column structure

--- components/layouts/ContentManagerLayout.js
+++ components/layouts/ContentManagerLayout.js
@@ -1,16 +1,14 @@
 import { AppContent, AppSidebar } from '@/components/elements/inner/index';
 import ClientSessionHandler from '@/components/ClientSessionHandler';
-import ContentManagerSubMenu from 'components/elements/inner/ContentManagerSubMenu';
 const ContentManagerLayout = ({children,currentTypeSlug}) => {
   return (
     <ClientSessionHandler>
         <AppSidebar />
         <div className="wrapper d-flex align-items-start justify-content-start min-vh-100 bg-light">
-          <ContentManagerSubMenu title='Content' currentTypeSlug={currentTypeSlug}/>
-            <div className="body flex-grow-1 px-3 main-container">
-              <AppContent>
-                    {children}
-              </AppContent>
+          <div className="body flex-grow-1 px-3 main-container">
+            <AppContent>
+                  {children}
+            </AppContent>
         </div>
         </div>
   </ClientSessionHandler>
