a5f3b2cf89854ca0ac568330fe42deff9b1e4412
Ardee Aram <ardee.aram@gmail.com>
2023-04-06 17:46:54 +0800

    Migrations successfully sent to the database!

--- scripts/migrations-processor.js
+++ scripts/migrations-processor.js
@@ -3,10 +3,18 @@ const {default: DB} = require('@klaudsol/commons/lib/DB');
 let buffer = '';
 
 const main = async (buffer) => {
+  const db = new DB();
   const reducedMigrations = JSON.parse(buffer);
   for(let i = 0; i < reducedMigrations.data.length; i++) {
-    console.log(reducedMigrations.data[i].name);
-    console.log(reducedMigrations.data[i].up);
+    let commandUp =reducedMigrations.data[i].up; 
+    console.error(reducedMigrations.data[i].name);
+    if(Array.isArray(commandUp)) commandUp = commandUp.join('')
+    console.error(">>>SQL:");
+    console.error(commandUp);
+    const result = await db.executeStatement(commandUp);
+    console.error(">>>RESULT:");
+    console.error(JSON.stringify(result));
+    console.error("\n");
   }
 }
 
