671b74a30e86da82392ea8aa12c2f5f713ea678c
johnservo <jaservo@up.edu.ph>
2023-04-10 16:54:38 +0800

    Transformed signup to users

---
+++ pages/api/admin/users/index.js
@@ -0,0 +1,22 @@
+import { handleRequests } from "@klaudsol/commons/lib/API";
+import { withSession } from "@klaudsol/commons/lib/Session";
+import { OK } from '@klaudsol/commons/lib/HttpStatuses';
+import InsufficientDataError from '@klaudsol/commons/errors/InsufficientDataError';
+import People from '@klaudsol/commons/models/People';
+
+export default withSession(handleRequests({ post }));
+
+async function post(req, res) {
+    const { firstName, lastName, loginEnabled, email, password, confirmPassword, forcePasswordChange } = req.body;
+
+    if (!firstName) throw new InsufficientDataError('Please enter your first name.');
+    if (!lastName) throw new InsufficientDataError('Please enter your last name.');
+    if (!email) throw new InsufficientDataError('Please enter your email.');
+    if (!password) throw new InsufficientDataError('Please enter your first password.');
+    if (!confirmPassword) throw new InsufficientDataError('Please confirm your password.');
+    if (password !== confirmPassword) throw new InsufficientDataError('The passwords do not match.');
+
+    // await People.createUser({ firstName, lastName, loginEnabled, email, password, forcePasswordChange });
+
+    res.status(OK).json({ message: 'Signup successful!' });
+}
