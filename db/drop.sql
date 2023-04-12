21c037f460c6462b8c0e043357cb08ec9f377b6f
Ardee Aram <ardee.aram@gmail.com>
2023-04-12 09:57:06 +0800

    Update cleanup script

--- db/drop.sql
+++ db/drop.sql
@@ -1,6 +1,18 @@
+/* This is a convenience cleanup script. This is not executed automatically.
+We leave the risk taking to the user.
+*/ 
+
 DROP TABLE IF EXISTS `attributes`;
 DROP TABLE IF EXISTS `entities`;
 DROP TABLE IF EXISTS `entity_types`;
 DROP TABLE IF EXISTS `people`;
 DROP TABLE IF EXISTS `sessions`;
 DROP TABLE IF EXISTS `values`;
+DROP TABLE IF EXISTS `settings`;
+DROP TABLE IF EXISTS `capabilities`;
+DROP TABLE IF EXISTS `groups`;
+DROP TABLE IF EXISTS `people_groups`;
+DROP TABLE IF EXISTS `group_capabilities`;
+DROP TABLE IF EXISTS `migrations`;
+DROP TABLE IF EXISTS `system`;
+
