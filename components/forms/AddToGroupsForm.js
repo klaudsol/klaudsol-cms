641e040f82da94fb611fd7767db400ffc36e692a
johnservo <jaservo@up.edu.ph>
2023-04-21 10:05:01 +0800

    Turned group filters into reduce

--- components/forms/AddToGroupsForm.js
+++ components/forms/AddToGroupsForm.js
@@ -8,8 +8,11 @@ export default function AddToGroupsForm({ groups }) {
     const { values, errors, touched } = useFormikContext();
     const { capabilities } = useContext(CacheContext);
 
-    const systemSupplied = groups.filter((group) => group.isSystemSupplied);
-    const userCreated = groups.filter((group) => !group.isSystemSupplied);
+    const { systemSupplied, userCreated } = groups.reduce((acc, group) => {
+        if (group.isSystemSupplied) return { ...acc, systemSupplied: [...acc.systemSupplied, group] }
+
+        return { ...acc, userCreated: [...acc.userCreated, group]}
+    }, { systemSupplied: [], userCreated: [] })
 
     return (
         <div>
