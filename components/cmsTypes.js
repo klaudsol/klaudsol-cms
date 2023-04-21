28b2328d1532367882d21ff1bb952d2f0946ce49
johnservo <jaservo@up.edu.ph>
2023-04-21 16:19:51 +0800

    typo

--- components/cmsTypes.js
+++ components/cmsTypes.js
@@ -6,7 +6,7 @@ export const CMS_TYPES = {
   IMAGE: "image",
   // Not sure if I should include the password and the checkbox here
   // since they are not attribute types. I included it so that
-  // The AdminRenderer component can function properly
+  // the AdminRenderer component can function properly
   PASSWORD: "password",
   CHECKBOX: "checkbox"
 };
