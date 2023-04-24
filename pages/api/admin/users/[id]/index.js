cc228dac603baf921d27e5207abdff9aada7e2d1
johnservo <jaservo@up.edu.ph>
2023-04-24 17:30:38 +0800

    WIP

--- pages/api/admin/users/[id]/index.js
+++ pages/api/admin/users/[id]/index.js
@@ -49,6 +49,7 @@ async function put(req, res) {
 
     if (toAdd.length > 0) await PeopleGroups.connect({ id, groups: toAdd });
     if (toDelete.length > 0) await PeopleGroups.disconnect({ id, groups: toDelete });
+    // if approved false remove people groups
 
     res.status(OK).json({ message: 'Update successful!' });
 }
