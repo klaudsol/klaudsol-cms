48cfa0d20791c1518b7774abc75e1e267cf86620
johnservo <jaservo@up.edu.ph>
2023-04-26 15:22:32 +0800

    Removed readAdmin

--- components/elements/inner/AppSidebar.js
+++ components/elements/inner/AppSidebar.js
@@ -17,7 +17,7 @@ import CollapsedSidebar from './sidebar/CollapsedSidebar';
 import { SET_COLLAPSE } from '@/lib/actions';
 import RootContext from '@/components/contexts/RootContext';
 import { useCapabilities } from '@/components/hooks';
-import { writeSettings, writeContentTypes, readUsers,  readGroups, writeContents, readPendingUsers, readAdmin } from "@/lib/Constants";
+import { writeSettings, writeContentTypes, readUsers,  readGroups, writeContents, readPendingUsers } from "@/lib/Constants";
 import { loadEntityTypes } from '@/components/reducers/actions';
 import pluginMenus from '@/plugin-menus.json';
 
