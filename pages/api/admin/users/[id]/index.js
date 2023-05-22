ca034cc09d15b8d50aecf13494cf4e4adacda059
johnservo <jaservo@up.edu.ph>
2023-05-22 08:22:47 +0800

    Removed promise all for assertUserCan

--- pages/api/admin/users/[id]/index.js
+++ pages/api/admin/users/[id]/index.js
@@ -33,15 +33,9 @@ async function put(req, res) {
     const { id } = req.query;
     const { firstName, lastName, forcePasswordChange, loginEnabled, approved, email, isSameEmail, toAdd, toDelete } = req.body;
 
-    // IDEA: We can modify assertUserCan to cater to multiple capabilities so that
-    // it will only have to read the database once. However, it might be hard if we 
-    // need to use assertUserCan in certain conditions 
-    // ex. items 1 and 2 (start w/ 0) on the array inside Promise.all below
-    await Promise.all([
-        await assertUserCan(writeUsers, req),
-        ((toAdd.length > 0 || toDelete.length > 0) && assertUserCan(writeGroups, req)),
-        ((toAdd.includes(SUPER_ADMIN_ID.toString()) || toDelete.includes(SUPER_ADMIN_ID.toString())) && assertUserCan(promoteToSuperAdmin, req))
-    ])
+    await assertUserCan(writeUsers, req) &&
+        ((toAdd.length > 0 || toDelete.length > 0) && await assertUserCan(writeGroups, req)) &&
+        ((toAdd.includes(SUPER_ADMIN_ID.toString()) || toDelete.includes(SUPER_ADMIN_ID.toString()))) && await assertUserCan(promoteToSuperAdmin, req)
 
     if (!firstName) throw new InsufficientDataError('Please enter your first name.');
     if (!lastName) throw new InsufficientDataError('Please enter your last name.');
--- pages/api/admin/users/index.js
+++ pages/api/admin/users/index.js
@@ -43,11 +43,9 @@ async function post(req, res) {
         forcePasswordChange = false
     } = req.body;
 
-    await Promise.all([
-        assertUserCan(writeUsers, req),
-        (groups.length > 0 && assertUserCan(writeGroups, req)),
-        (groups.includes(SUPER_ADMIN_ID.toString()) && assertUserCan(assignToGroup, req, SUPER_ADMIN_ID))
-    ]);
+    assertUserCan(writeUsers, req) &&
+        (groups.length > 0 && await assertUserCan(writeGroups, req)) &&
+        (groups.includes(SUPER_ADMIN_ID.toString()) && await assertUserCan(assignToGroup, req, SUPER_ADMIN_ID))
 
     if (!firstName) throw new InsufficientDataError('Please enter your first name.');
     if (!lastName) throw new InsufficientDataError('Please enter your last name.');
