97175143feb6dc4d1b8ad3e3041d7247b530ee90
Ardee Aram <ardee.aram@gmail.com>
2023-06-13 18:15:05 +0800

    Created AttributeType base class

--- components/attribute_types/AttributeType.js
+++ components/attribute_types/AttributeType.js
@@ -1,3 +1,40 @@
+//This is the base class of all attribute types.
+//All attribute types must inherit from this class.
 export default class AttributeType {
+
+    constructor({data, metadata}) {
+      this.data = data;
+      this.metadata = metadata;
+    }
+
+    //Render this value in a Grid View
+    //This returns a React component.
+    renderGridView() {
+
+    }
+
+    //Render this value in Icon View
+    //This returns a React component.
+    renderIconView() {
+
+    }
+
+    //Render this value in the Admin
+    //This returns a React component.
+    renderAdmin() {
+
+    }
+
+    //Output as part of the final API response
+    //This returns an object
+    toApi() {
+
+    }
+
+    //serialize / commit to database, via EAV or other means.
+    //returns boolean, true if the data has been successfully commited.
+    save() {
+
+    }
 
 };
