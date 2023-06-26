578d9b6b0de232ea4d766fa54523b78e3542f8db
Ardee Aram <ardeearam@users.noreply.github.com>
2023-06-27 05:27:11 +0800

    Preserve AttributeType constructor signature

--- components/EntityAttributeValue.js
+++ components/EntityAttributeValue.js
@@ -43,13 +43,11 @@ const formatImage = (key) => {
       case 'custom':
         const CustomAttributeType = plugin(item.attributes_custom_name);
         const customAttributeType = new CustomAttributeType({
-          data: {
-            value: item.value_long_string, 
-            id: item.id
-          },
+          data: item.value_long_string, 
           metadata: {
             type: 'custom',
-            custom_name: item.attributes_custom_name
+            custom_name: item.attributes_custom_name,
+            id: item.id
           }
         });
 
